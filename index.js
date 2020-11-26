418c221fca6d1d0fc72bb1bbaa39f2cbaf088bd8
Alejandro C. Saksida <acsaksida@gmail.com>
2020-11-26 08:13:42 +0100

    Cleaning up code and improving suggestions

--- index.js
+++ index.js
@@ -4,81 +4,78 @@ const createEnturService = require('@entur/sdk').default
 const TypeName = require('@entur/sdk').TypeName
 
 
-var express = require('express');
-var app = express();
-
-/*const express = require('express')
-const path = require('path')
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
+var express = require('express')
+var app = express()
 
 const service = createEnturService({ clientName: 'alejandrosaksida-ruter-sirishortcuts' })
 
+app.get('/', function (req, res) {
+    res.send('Welcome to the Siri shortcuts app!');
+});
 
-app.get('/berg', function (req, res) {
-  //res.send('Hello World!');
-
+app.get('/metro-with-coords-from-to', function (req, res) {
   //Berg: NSR:StopPlace:6168 mot sentrum
   //Nationaltheatret: NSR:StopPlace:4067 mot east
 
-  /*service.getTripPatterns({from: 
-    {
-        coordinates: {
-            latitude:59.951888,longitude:10.744455
-        }
-},
-to: {
-    coordinates:{latitude: 59.9143454, longitude: 10.7284128}
-},
-modes: ['foot', 'metro']
+    service.getTripPatterns({from: 
+        {
+            coordinates: {
+                latitude:59.951888,longitude:10.744455
+            }
+        },
+        to: {
+            coordinates:{latitude: 59.9143454, longitude: 10.7284128}
+        },
+        modes: ['foot', 'metro']
+    })
+    .then(result=>{
+        console.log(result);
+        res.send(result)
+    })
 
-})
-  .then(result=>{
-    console.log(result);
-    res.send(result)
-})*/
+});
 
-  /*service.getNearestPlaces({latitude: 59.951904, longitude: 10.7409389},
-    {
-        filterByPlaceTypes: [TypeName.STOP_PLACE],
-        maximumDistance: 500,
-    },
+app.get('/metro-nearest-places', function (req, res) {
+    //Berg: NSR:StopPlace:6168 mot sentrum
+    //Nationaltheatret: NSR:StopPlace:4067 mot east
+
+    service.getNearestPlaces({latitude: 59.951904, longitude: 10.7409389},
+        {
+            filterByPlaceTypes: [TypeName.STOP_PLACE],
+            maximumDistance: 500,
+        },
     ).then(result=>{
         console.log(result);
         res.send(result)
-    })*/
+    })
+});
+
+app.get('/berg', function (req, res) {
+  
   service.getDeparturesBetweenStopPlaces('NSR:StopPlace:6168', 'NSR:StopPlace:4067').then(result=>{
     
-
     const arrivalTimes = result.map(elem=>elem.expectedDepartureTime)
 
     const a = new Date(); // Current date now.
-    const b = new Date(arrivalTimes[0]); // Start of 2010.
+    const b = new Date(arrivalTimes[0]); // The first coming metro
 
     const d = (b-a); // Difference in milliseconds.
     const seconds = parseInt((b-a)/1000);
     const minutes = parseInt(seconds / 60);
 
     const secondsOfMinutes = seconds - minutes * 60;
-    //console.log(result);
-    //res.send({time: arrivalTimes[0], current:a, b:b, d:minutes+" minutes from now"});
-    let response = "Berg to center leaves in "+minutes+" minutes and "+ secondsOfMinutes + "seconds from now.";
-    if (seconds < 70) response += "You'll have to dash to reach it."
-    else if (seconds < 60 * 2) response += "Run Forest, Run!"
-    else if (seconds < 60 * 4) response += "Leave now and power-walk."
-    else if (seconds < 60 * 6) response += "You'll catch walking it if you leave now."
-    else response += "You can still wait " + minutes - 6 +" minutes, but don't fall asleep."
+
+    let response = "Berg to center leaves in "+minutes+" minutes and "+ secondsOfMinutes + " seconds from now.";
+    if (seconds < 70) response += " You'll have to dash to reach it."
+    else if (seconds < 60 * 4) response += " Run Forest, Run!"
+    else if (seconds < 60 * 6) response += " Leave now and power-walk."
+    else if (seconds < 60 * 8) response += " You'll catch walking it if you leave now."
+    else response += (" You can still wait " + (minutes - 6) +" minutes, but don't fall asleep.")
     res.send(response);
 });
 
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port '+PORT+'!');
+  console.log('Siri shortcuts app listening on port '+PORT+'!');
 });
